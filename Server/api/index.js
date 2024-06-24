f6d10044ed065ca3eb196ba8b98a4854ed2c520e
subhashsha15 <subhashsha15@gmail.com>
2024-06-24 19:57:06 +0530

    Update index.js

--- Server/api/index.js
+++ Server/api/index.js
@@ -40,6 +40,12 @@ const connectDB = async () => {
     }
 }
 
+// Middleware to log requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 // Routes
 app.use('/api/auth', authRoute);
 app.use('/api/breweries', breweryRoute);
