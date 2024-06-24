e2b606e01922e1f65c80d66906d103bcdd6cc07c
subhashsha15 <subhashsha15@gmail.com>
2024-06-24 17:56:14 +0530

    update vercel.json

--- Server/api/index.js
+++ Server/api/index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 
 
 //Middlewares
-app.use(cors({ origin: ["http://localhost:5173"],methods:["POST","GET"], credentials: true }));
+app.use(cors({ origin: ["https://mo-engage-client.vercel.app"],methods:["POST","GET"], credentials: true }));
 app.use(express.json());
 
 const port = 3000;
